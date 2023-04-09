7c2acc0094e37760cccc75c248790c0675432fe5
six-icicle <130260421+six-icicle@users.noreply.github.com>
2023-04-08 18:46:56 -0700

    Update streamlit_app.py
    changed typo in line 17

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # inserting ability for user to determine fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-fruits_to_show = my_fruits_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on the page
 streamlit.dataframe(fruits_to_show)
