49a3eeb8c9a50cb36bc2a409e35a74de1e9270d2
six-icicle <130260421+six-icicle@users.noreply.github.com>
2023-04-08 18:41:36 -0700

    Update streamlit_app.py
    added multiselect for user to pick fruit
    displaying fruit still underneath multiselect now

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# inserting ability for user to determine fruit they want to include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# display the table on the page
 streamlit.dataframe(my_fruit_list)
